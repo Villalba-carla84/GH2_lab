0e9cfa4d6f3501991403272d79a39d6722ae7730
carlavillalba <carlavillalba84@hotmail.com>
2021-06-27 02:38:20 -0300

    modifique codigo

--- uno.cpp
+++ uno.cpp
@@ -4,6 +4,6 @@ int main(){
 cout << " hola mundo" << endl;
 cout << "segundo commit "<< endl;
 cout << "tercer commit"<< endl;
-cout << " estoy en la rama AyEDII"
+cout << " estoy en la rama AyEDII y modifique";
 return 0;
 }
